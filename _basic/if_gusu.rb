85c31d8247ff9bb75d52569061e619b0dc5b2285
inamuu <8310973+inamuu@users.noreply.github.com>
2020-04-07 00:56:47 +0900

    :gem: if文の条件式

--- _basic/if_gusu.rb
+++ _basic/if_gusu.rb
@@ -1,5 +1,5 @@
 
-puts "数字を入力してください: "
+print "数字を入力してください: "
 num = gets.to_i
 
 if num % 2 == 0
@@ -8,8 +8,19 @@ else
   puts "奇数です"
 end
 
-puts "数字を入力してくださいパート２:"
+print "数字を入力してくださいパート２: "
 num2 = gets.to_i
 
 puts "偶数です" if num % 2 == 0
+
+print "数字を入力してくださいパート３: "
+num3 = gets.to_i
+
+if num3 >= 10
+  puts "送料無料"
+elsif 0 < num3 && num3 < 10
+  puts "送料100円です"
+else
+  puts "入力に誤りがあります"
+end
 
